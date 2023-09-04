86bb9863dfa34632552e27b2e198700c1c887c1b
usern4353 <user33333333@mail.com>
2023-09-04 03:16:25 -0400

    fix switch

--- projects/osbo-sense/src/lib/osbo-sense.component.ts
+++ projects/osbo-sense/src/lib/osbo-sense.component.ts
@@ -7,26 +7,29 @@ import { OsboSenseService } from './osbo-sense.service';
   selector: 'ngosbo-adsense',
   template: `
 <div #dov>
-    <ng-container *ngIf="this.config.tipo=='display'">
+  <ng-container [ngSwitch]="this.config.tipo">
+    <ng-container *ngSwitchCase="'display'">
         <ins class="adsbygoogle" attr.style="{{this.config.style}}" attr.data-ad-client="{{this.config.dataadclient}}"
             attr.data-ad-slot="{{this.config.dataadslot}}" attr.data-ad-format="{{this.config.dataadformat}}"
             attr.data-full-width-responsive="{{this.config.datafullwidthresponsive}}">
         </ins>
-    </ng-container>
-    <ng-container *ngIf="this.config.tipo=='infeed'">
+    </ng-container>    
+    <ng-container *ngSwitchCase="'infeed'">
         <ins class="adsbygoogle" attr.style="{{this.config.style}}" attr.data-ad-client="{{this.config.dataadclient}}"
             attr.data-ad-slot="{{this.config.dataadslot}}" attr.data-ad-format="{{this.config.dataadformat}}"
             attr.data-ad-layout-key="{{this.config.dataadqlayoutkey}}">
         </ins>
     </ng-container>
-    <ng-container *ngIf="this.config.tipo=='multiple'">
+    <ng-container *ngSwitchCase="'multiple'">
         <ins class="adsbygoogle" attr.style="{{this.config.style}}" attr.data-ad-client="{{this.config.dataadclient}}"
             attr.data-ad-slot="{{this.config.dataadslot}}" attr.data-ad-format="{{this.config.dataadformat}}">
         </ins>
     </ng-container>
-    <ng-container *ngIf="this.config.tipo!='infeed' && this.config.tipo!='display' && this.config.tipo!='multiple'">
+    <ng-container *ngSwitchDefault>
         tipo de anuncio no permitido
     </ng-container>
+  </ng-container>
+
 </div>
   `,
   styles: [
@@ -36,7 +39,7 @@ export class OsboSenseComponent implements AfterViewInit {
   @ViewChild("dov") divi?: ElementRef;
   @Input() config!: SenseConfiguration;
   constructor(private scriptservice: OsboSenseService) {
-    
+
   }
   ngAfterViewInit() {
     this.scriptservice.executeScript("(adsbygoogle = window.adsbygoogle || []).push({});", this.divi);
